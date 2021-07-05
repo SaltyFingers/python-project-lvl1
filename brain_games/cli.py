5a9e4dafefa4952f0e812c1cfca01f96619162f6
Evchik <elcric66@gmail.com>
2021-07-05 17:49:22 +0300

    step 5 completed

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,6 +3,9 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
-    welcome = print('Hello, ' + name + '!')
-    return welcome
+    name = prompt.string('Hello there!\nMay I have your name, friend?\nMy name is ')
+    print('Nice to meet you, ' + name + '!')
+    return name 
+
+if __name__ == "__main__":
+    welcome_user()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from brain_games.logic import even_logic
+
+def main():
+    even_logic()
+
+
+if __name__ == "__main__":
+    main()
