f7594ecd3c5daabce76fff18d821dd795f62e035
Evchik <elcric66@gmail.com>
2021-07-13 00:19:43 +0300

    edit game_calc 3

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -30,8 +30,8 @@ def calc_logic():
         number_2 = random_number()
         operator_key = choice(operators_list)
         current_operator = operators.get(operator_key)
-        questions.append(
-        str(number_1) + ' ' + operator_key + ' ' + str(number_2))
+        questions.append(str(number_1) + '\
+         ' + operator_key + ' ' + str(number_2))
         correct_answers.append(current_operator(number_1, number_2))
 
     engine(name=name, questions=questions, correct_answers=correct_answers)
