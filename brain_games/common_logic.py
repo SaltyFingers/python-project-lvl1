b9949783500af269d8693ba6680ba532513d5b7a
Evchik <elcric66@gmail.com>
2021-07-22 23:27:38 +0300

    edit common_logic.py

--- brain_games/common_logic.py
+++ brain_games/common_logic.py
@@ -8,7 +8,7 @@ def welcome():
     print('Welcome to the Brain Games!')
 
 
-def welcome_user():
+def user_name():
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
     return name
@@ -16,8 +16,9 @@ def welcome_user():
 
 def engine(game):
     welcome()
-    name = welcome_user()
+    name = user_name()
     print(game.TASK)
+    right_answers = 0
     for current_round in range(ROUNDS):
         game_data = game.game_logic()
         question, correct_answer = game_data[0], game_data[1]
@@ -25,10 +26,11 @@ def engine(game):
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print('Correct!')
+            right_answers += 1
         else:
             print("'" + answer + "' is the wrong answer :c. \
 Correct answer was '" + correct_answer + "'\
 \nLet\'s try again, " + name + '!')
             break
-        if current_round == ROUNDS - 1:
+        if right_answers == ROUNDS:
             print('Congratulations, ' + name + '!')
