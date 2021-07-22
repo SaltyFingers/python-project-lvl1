c87fdcb62c272c6cb22ce438e6a5b32add4d6799
Evchik <elcric66@gmail.com>
2021-07-23 00:09:25 +0300

    edit condition for 1 to game_prime.py

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -6,15 +6,14 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(number):
     x = 0
-    if number > 1:
-        for j in range(2, number // 2 + 1):
-            if (number % j == 0):
-                x += 1
-        if x <= 0:
-            correct_answer = 'yes'
-        else:
-            correct_answer = 'no'
-    else: 
+    for j in range(2, number // 2 + 1):
+        if (number % j == 0):
+            x += 1
+    if x <= 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    if number == 1:
         correct_answer = 'no'
     return correct_answer
 
